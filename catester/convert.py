9516d6f9dfad469b033aa608eb0f34315f70c525
trigonal <michael.meierhofer@student.tugraz.at>
2024-02-19 11:01:03 +0100

    .

--- catester/convert.py
+++ catester/convert.py
@@ -12,6 +12,6 @@ if __name__ == "__main__":
     parser.add_argument("--verbosity", default=0, help="test verbosity level 0, 1, 2 or 3")
     parser.add_argument("--metayaml", default=None, help="abs/rel path to initial meta.yaml")
     parser.add_argument("--formatter", action='store_true', help="use black as formatter for the reference-file")
-    parser.add_argument("--testdirs", choices=["all", "none", "correct", "empty"], default="all", help="run converter, run tests or all")
+    parser.add_argument("--testdirs", choices=["all", "none", "correct", "empty"], default="all", help="local test directories to include for action")
     args = parser.parse_args()
     convert_master(args.scandir, args.action, args.verbosity, args.metayaml, args.formatter, args.testdirs)
