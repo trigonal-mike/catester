e841c4cdd993454bcef45370b4dbf97a4bef42e9
trigonal <michael.meierhofer@student.tugraz.at>
2024-02-20 13:57:18 +0100

    ...

--- ex_master/examples/new/ex_master.py
+++ ex_master/examples/new/ex_master.py
@@ -1,6 +1,7 @@
 #$VARIABLETEST test1
 #$TESTVAR x
 #$PROPERTY value 2
+#$TESTVAR y
 
 #$VARIABLETEST test2
 #$PROPERTY successDependency 1
@@ -9,3 +10,4 @@
 import numpy as np
 h=np.array([[[1,2,3],[3,4,5]],[[5,6,7],[7,8,9]],[[9,10,11],[12,13,14]]])
 x=1
+y=1
